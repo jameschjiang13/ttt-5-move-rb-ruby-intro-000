026e4516e5160298e7f4c9a1ed875f6ba3e82971
James Jiang <monochrome473@gmail.com>
2020-03-23 03:19:44 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -4,9 +4,7 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-index = 0
+end 
 
 def input_to_index(input)
   index = input.to_i - 1
